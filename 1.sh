0c33ac94249c381076646e3b74fcb936e9035a83
root <2532771263@qq.com>
2023-06-17 00:53:53 +0800

    fix

--- 1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# nginx_notes" >> README.md
-git init
-git add *
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/joedlut/nginx_notes.git
-git push -u origin master
